69d39acc97257a47ba0e7f6efe378274773e412f
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2025-05-30 22:45:30 +0200

    Initial Commit

---
+++ SwiftSourceControl/SwiftSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSourceControlApp.swift
+//  SwiftSourceControl
+//
+//  Created by Daniel A on 2025-05-30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
