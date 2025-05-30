4b7c6332c0b22c3f7eedb0b60a89906b6472900e
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2025-05-31 01:50:19 +0200

    [Patch] Fix subscribe button

--- SwiftSourceControl/ContentView.swift
+++ SwiftSourceControl/ContentView.swift
@@ -54,7 +54,7 @@ struct ContentView: View {
           Rectangle()
             Text("B")
             Button("Subscribe now!!"){
-                
+            // kanske fel
             
             }
             
