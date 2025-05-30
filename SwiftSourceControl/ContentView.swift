370b1a43961e372506f844afa3b0e6b72043f95c
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2025-05-31 00:56:51 +0200

    [Celan] Uppdates to documentation

--- SwiftSourceControl/ContentView.swift
+++ SwiftSourceControl/ContentView.swift
@@ -24,8 +24,16 @@
  MUNDANE TASK
  [Clean] description of changes
  
+ */
+
+/*
+  Clone = Copying the repro locally
+ Commit = Save "Checkpoint on current branch"
+ Stage = Prepare changes for a commit
+ Stash = Save changes for later
  
- 
+ Push = Send local commits to repro
+ Pull = Fecth remote commits to local repro
  
  
  */
