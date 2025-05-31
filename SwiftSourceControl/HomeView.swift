b58a9409e7cd1d232890304011ee4e12f0bd5eb2
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2025-06-01 00:27:38 +0200

    [Feature] Changed title

--- SwiftSourceControl/HomeView.swift
+++ SwiftSourceControl/HomeView.swift
@@ -16,7 +16,7 @@ struct HomeView: View {
         ZStack {
         Text("HI")
             
-            Text("Hello")
+            Text("Hello nick")
             
             VStack{
                 Text("Hello")
