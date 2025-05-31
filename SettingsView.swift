3c0b392c93c543f7075eaaacff11d5ab0c2acbec
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2025-06-01 01:12:30 +0200

    [Feature] Added SettingsView

---
+++ SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SwiftSourceControl
+//
+//  Created by Daniel A on 2025-06-01.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
